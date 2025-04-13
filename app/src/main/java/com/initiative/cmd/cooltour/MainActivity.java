328b14c5b594cc3bbeb5445ec47d311dad15be5f
V8086 <vmode8086@gmail.com>
2025-04-13 14:07:52 +0300

    App project created; added pages & pg switching

---
+++ app/src/main/java/com/initiative/cmd/cooltour/MainActivity.java
@@ -0,0 +1,106 @@
+package com.initiative.cmd.cooltour;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.widget.Toolbar;
+
+import androidx.activity.EdgeToEdge;
+import androidx.activity.OnBackPressedCallback;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+import com.initiative.cmd.cooltour.fragments.HistoryFragment;
+import com.initiative.cmd.cooltour.fragments.MapFragment;
+import com.initiative.cmd.cooltour.fragments.ProfileFragment;
+import com.initiative.cmd.cooltour.fragments.RoutesFragment;
+import com.initiative.cmd.cooltour.fragments.SettingsFragment;
+import com.initiative.cmd.cooltour.fragments.UsersFragment;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    DrawerLayout drawerLayout_;
+    NavigationView navigationView_;
+    BottomNavigationView bottomNavigationView_;
+    FragmentManager fragmentManager_;
+    Toolbar toolbar_;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        OnBackPressedCallback onBackPressedCallback = new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                if (drawerLayout_.isDrawerOpen(GravityCompat.START)) {
+                    drawerLayout_.closeDrawer(GravityCompat.START);
+                }
+                else {
+                    setEnabled(false);
+                    getOnBackPressedDispatcher().onBackPressed();
+                    setEnabled(true);
+                }
+            }
+        };
+        getOnBackPressedDispatcher().addCallback(onBackPressedCallback);
+
+        navigationView_ = findViewById(R.id.nav_view);
+        navigationView_.setNavigationItemSelectedListener(this);
+        navigationView_.setCheckedItem(R.id.nav_main);
+
+        toolbar_ = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar_);
+
+        drawerLayout_ = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(
+                this, drawerLayout_, toolbar_,
+                R.string.nav_drawer_open, R.string.nav_drawer_close
+        );
+        drawerLayout_.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        bottomNavigationView_ = findViewById(R.id.bottom_nav);
+        bottomNavigationView_.setBackground(null);
+        bottomNavigationView_.setOnItemSelectedListener(menuItem -> {
+            int item = menuItem.getItemId();
+
+            if (item == R.id.bottom_nav_menu_map) openFragment(new MapFragment());
+            else if (item == R.id.bottom_nav_menu_routes) openFragment(new RoutesFragment());
+            else if (item == R.id.bottom_nav_menu_history) openFragment(new HistoryFragment());
+            else if (item == R.id.bottom_nav_menu_users) openFragment(new UsersFragment());
+
+            return true;
+        });
+
+        fragmentManager_ = getSupportFragmentManager();
+        openFragment(new MapFragment());
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        int item = menuItem.getItemId();
+
+        if (item == R.id.nav_main) openFragment(new MapFragment());
+        else if (item == R.id.nav_profile) openFragment(new ProfileFragment());
+        else if (item == R.id.nav_settings) openFragment(new SettingsFragment());
+
+        drawerLayout_.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    private void openFragment(Fragment fragment) {
+        FragmentTransaction transaction = fragmentManager_.beginTransaction();
+        transaction.replace(R.id.fragment_container, fragment);
+        transaction.commit();
+    }
+}
